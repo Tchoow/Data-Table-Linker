b1b8dfde22eb88aa66237d7bc09150dca20638dd
Theo Lavie <theo.l@studiosport.fr>
2021-12-04 21:22:22 +0100

    init

---
+++ script.js
@@ -0,0 +1,40 @@
+/*
+    Table Linker
+    ---------------------------
+    Theo ~ @Tchoow 04/12/2021
+
+*/
+
+/*
+field of a component
+String name;
+String type;
+int    order;
+Component compatibliity;
+*/
+
+
+const btnAdd      = document.getElementById('btnAdd');
+var allComponents = [];
+
+
+var Component = function (name, type, order) {
+    this.name          = name;
+    this.type          = type;
+    this.order         = order;
+    this.compatibility = [];
+
+    this.addCompatibility = function(compatibility) {
+        this.compatibility.push(compatibility);
+    }
+
+}
+
+
+allComponents.push( new Component('Chassis Apex', 'Frame', 0));
+allComponents.push( new Component('Motor T-max', 'Moror', 4));
+
+// add a compatibility to the first component
+allComponents[0].addCompatibility(allComponents[1]);
+
+console.log(allComponents);
